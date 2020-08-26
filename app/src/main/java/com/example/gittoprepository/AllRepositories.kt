81d056f086ab3e5b0a3ea6f10cd3ed0ef9f1b4bc
Rahul yadav <ydvrahul8@gmail.com>
2020-08-26 16:15:29 +0530

    Fetch all repositories completed

--- app/src/main/java/com/example/gittoprepository/AllRepositories.kt
+++ app/src/main/java/com/example/gittoprepository/AllRepositories.kt
@@ -3,10 +3,15 @@ package com.example.gittoprepository
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.gittoprepository.adapters.RepositoryAdapter
+import com.example.gittoprepository.models.Repository
 import com.example.gittoprepository.viewmodels.AllRepositoriesViewModel
+import kotlinx.android.synthetic.main.activity_all_repositories.*
 
 class AllRepositories : AppCompatActivity() {
 
@@ -23,7 +28,16 @@ class AllRepositories : AppCompatActivity() {
 
     private fun repositoryObserver() {
         viewModel?.allRepository?.observe(this, Observer {
-            Log.e(TAG, "repositoryObserver: ${it.size} and "+it.toString() )
-        })
+            Log.e(TAG, "repositoryObserver: ${it.size} and " + it.toString())
+            if (!it.isNullOrEmpty()) {
+                recyclerView.layoutManager = LinearLayoutManager(this)
+                recyclerView.setHasFixedSize(true)
+                recyclerView.adapter = RepositoryAdapter(it) { position ->
+                    Toast.makeText(this, "Data is $position", Toast.LENGTH_SHORT).show()
+                }
+            } else
+                Toast.makeText(this, "Sorry no data found...", Toast.LENGTH_SHORT).show()
+        }
+        )
     }
 }
---
+++ app/src/main/java/com/example/gittoprepository/adapters/RepositoryAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.gittoprepository.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.gittoprepository.R
+import com.example.gittoprepository.models.Repository
+
+class RepositoryAdapter(
+    val list: MutableList<Repository>,
+    var listener: (Int) -> Unit
+) : RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder>() {
+    inner class RepositoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(position: Int, listener: (Int) -> Unit) {
+            Glide.with(itemView)
+                .load(list[position].owner.avatar_url)
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .into(image)
+            name.text = list[position].name
+            full_name.text = list[position].full_name
+            layout.setOnClickListener { listener(position) }
+        }
+
+        var image = view.findViewById<ImageView>(R.id.imageView_avatar)
+        var name = view.findViewById<TextView>(R.id.textView_name)
+        var full_name = view.findViewById<TextView>(R.id.textView_fullName)
+        var layout = view.findViewById<CardView>(R.id.view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_repository, parent, false)
+        return RepositoryViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = 20
+
+    override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) {
+        holder.bind(position, listener)
+    }
+
+}
